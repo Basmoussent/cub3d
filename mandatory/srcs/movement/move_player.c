f54efd03e16fa8222da73ccc7db058db46dcf480
Anton G <antongozlan@hotmail.com>
2025-03-07 13:46:28 +0100

    Joueur bouge (encore pas tres fluide)

--- mandatory/srcs/movement/move_player.c
+++ mandatory/srcs/movement/move_player.c
@@ -6,22 +6,22 @@
 /*   By: agozlan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 12:29:02 by agozlan           #+#    #+#             */
-/*   Updated: 2025/03/06 22:55:07 by agozlan          ###   ########.fr       */
+/*   Updated: 2025/03/07 13:43:49 by agozlan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	move(s_game *game, int index, int vector_x, int vector_y)
+void	move(s_game *game, int index, double vector_x, double vector_y)
 {
 	// 1.2 = player->speed, voir comment implementer
 	(void)index;
 	//	while(game->key_bool[index] == 1)
 //  {
-    if (game->map[(int)(game->p->pos_y)][(int)(game->p->pos_x + vector_x)] != 1)
-	  	game->p->pos_x += vector_x;
-  	if (game->map[(int)(game->p->pos_y + vector_y)][(int)game->p->pos_x] != 1)
-		  game->p->pos_y += vector_y;
+    if (game->map[(int)game->p->pos_x][(int)vector_x] != 1)
+	  	game->p->pos_x = vector_x;
+  	if (game->map[(int)vector_y][(int)game->p->pos_x] != 1)
+		  game->p->pos_y = vector_y;
 	  rendering(game);
 //    usleep(200); // aleatoire
 //  }
@@ -29,12 +29,12 @@ void	move(s_game *game, int index, int vector_x, int vector_y)
 
 void	move_player(s_game *game, int keycode)
 {
-	if (keycode == 119)
-		move(game, 0, game->p->dir_x * 1.2, game->p->dir_y * 1.2);
-	else if (keycode == 115)
-		move(game, 1, -game->p->dir_x * 1.2, -game->p->dir_y * 1.2);
-	else if (keycode == 97)
-		move(game, 2, -game->p->plane_x * 1.2, -game->p->plane_y * 1.2);
-	else if (keycode == 100)
-		move(game, 3, game->p->plane_x * 1.2, game->p->plane_y * 1.2);
+	if (keycode == W)
+		move(game, 0, game->p->pos_x + game->p->dir_x * MOVESPEED, game->p->pos_y + game->p->dir_y * MOVESPEED);
+	else if (keycode == S)
+		move(game, 1, game->p->pos_x - game->p->dir_x * MOVESPEED, game->p->pos_y - game->p->dir_y * MOVESPEED);
+	else if (keycode == A)
+		move(game, 2, game->p->pos_x + game->p->dir_y * MOVESPEED, game->p->pos_y - game->p->dir_x * MOVESPEED);
+	else if (keycode == D)
+		move(game, 3, game->p->pos_x - game->p->dir_y * MOVESPEED, game->p->pos_y + game->p->dir_x * MOVESPEED);
 }
