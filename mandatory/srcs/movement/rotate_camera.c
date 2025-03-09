b57da049b8b25877208fb3bdbc956d02b77e667b
Anton G <antongozlan@hotmail.com>
2025-03-09 15:25:33 +0100

    Ne traverse plus les murs (glisse) + mise au propre

--- mandatory/srcs/movement/rotate_camera.c
+++ mandatory/srcs/movement/rotate_camera.c
@@ -6,7 +6,7 @@
 /*   By: agozlan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 12:31:22 by agozlan           #+#    #+#             */
-/*   Updated: 2025/03/07 16:14:34 by agozlan          ###   ########.fr       */
+/*   Updated: 2025/03/09 15:21:23 by agozlan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,17 @@ To rotate a vactor, simply multiply it with the rotation matrix
 (where a is the magnitude of rotation)
 */
 
-void	rotation(s_game *game, int index, double a)
+void	rotation(s_game *game, double a)
 {
 	double	tmp;
 
-	(void)index;
-//	while (game->key_bool[index] == 1)
-//	{
-		tmp = game->p->dir_x;
-		game->p->dir_x = game->p->dir_x * cos(a) - game->p->dir_y * sin(a);
-		game->p->dir_y = tmp * sin(a) + game->p->dir_y * cos(a);
-		tmp = game->p->plane_x;
-		game->p->plane_x = game->p->plane_x * cos(a) - game->p->plane_y * sin(a);
-		game->p->plane_y = tmp * sin(a) + game->p->plane_y * cos(a);
-		rendering(game);
-//		usleep(200); pas bon car tout le programme dort
-//	}
+	tmp = game->p->dir_x;
+	game->p->dir_x = game->p->dir_x * cos(a) - game->p->dir_y * sin(a);
+	game->p->dir_y = tmp * sin(a) + game->p->dir_y * cos(a);
+	tmp = game->p->plane_x;
+	game->p->plane_x = game->p->plane_x * cos(a) - game->p->plane_y * sin(a);
+	game->p->plane_y = tmp * sin(a) + game->p->plane_y * cos(a);
+	rendering(game);
 }
 
 // Left rotate -1    / Right rotate 1
@@ -42,7 +37,7 @@ void	rotation(s_game *game, int index, double a)
 void	rotate_camera(s_game *game)
 {
 	if (game->key_bool[4])  // left
-		rotation(game, 4, ROTSPEED * -1); // valeur aleatoire ici
+		rotation(game, ROTSPEED * -1); // valeur aleatoire ici
 	if (game->key_bool[5])
-		rotation(game, 5, ROTSPEED); // valeur aleatoire
+		rotation(game, ROTSPEED); // valeur aleatoire
 }
