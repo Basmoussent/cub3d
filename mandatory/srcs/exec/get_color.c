c57dbcc7bf6ad7e5f63378e51de175b1d1a0c0db
agozlan <agozlan@student.42.fr>
2025-03-10 10:04:26 +0100

    Textures ok

--- mandatory/srcs/exec/get_color.c
+++ mandatory/srcs/exec/get_color.c
@@ -45,38 +45,21 @@ int	get_textures(s_game *game, s_rayon *rayon, int **buffer, int x, int y)
 
 	tex_dir = find_side(game, rayon);
 	texX = (int)(rayon->wall_x * (double)tex_dir->width);
-	if (rayon->side == 0 && rayon->dir_x > 0)
-		texX = tex_dir->width - texX - 1;
-	if (rayon->side == 1 && rayon->dir_y < 0)
-		texX = tex_dir->width - texX - 1;
-	step = 1.0 * tex_dir->height / rayon->line_height;
+	if ((rayon->side == 0 && rayon->dir_x < 0) || (rayon->side == 1 && rayon->dir_y > 0))
+    texX = tex_dir->width - texX -1;
+
+  step = 1.0 * tex_dir->height / rayon->line_height;
 	texPos = (rayon->draw_start - WIN_HEIGHT / 2 + rayon->line_height / 2) * step;
-	while (y < rayon->draw_end)
+  y = rayon->draw_start;
+  while (y < rayon->draw_end)
 	{
-		texY = (int)texPos & (tex_dir->height - 1);
+		texY = (int)texPos % tex_dir->height;
 		texPos += step;
 		buffer[y][x] = *(int *)(tex_dir->addr + ((texY * tex_dir->line_length) + texX * (tex_dir->bits_per_pixel / 8)));
 		y++;
 	}
 	return (y - 1);
 }
-
-/*
-// fonction nulle pour continuer
-int	get_textures(s_game *game, s_rayon *rayon, int **buffer, int x, int y)
-{
-	(void)game;
-	while (y < rayon->draw_end)
-	{
-		if (rayon->side)
-			buffer[y][x] = 255;
-		else
-			buffer[y][x] = 3277055;
-		y++;
-	}
-	return (y - 1);
-}
-*/
 
 void	get_color(s_game *game, s_rayon *rayon, int **buffer, int x)
 {
