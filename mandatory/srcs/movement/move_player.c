4f0648e7dacf265bff7a4f48416227c14574647b
agozlan <agozlan@student.42.fr>
2025-03-12 12:37:34 +0100

    Gere probleme rentre dans le mur

--- mandatory/srcs/movement/move_player.c
+++ mandatory/srcs/movement/move_player.c
@@ -11,29 +11,33 @@
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
+/*
 static int is_wall_or_door(char c)
 {
 	return (c == '1' || c == 'D');  // La porte fermée agit comme un mur
 }
-
+*/
 void	move(t_game *game, double vector_x, double vector_y)
 {
 	double	reste;
 	double	unused;
 
-	if (game->map[(int)game->p->pos_y][(int)vector_x] != '1' && game->map[(int)game->p->pos_y][(int)vector_x] != 'D')
+	if (game->map[(int)game->p->pos_y][(int)vector_x] != '1'
+    && game->map[(int)game->p->pos_y][(int)vector_x] != 'D')
 	{
 		game->p->pos_x = vector_x;
 		reste = modf(vector_x, &unused);
-		if ((!reste && game->map[(int)game->p->pos_y][(int)vector_x - 1] == '1') || (!reste && game->map[(int)game->p->pos_y][(int)vector_x - 1] == 'D'))
+		if ((!reste && game->map[(int)game->p->pos_y][(int)vector_x - 1] == '1')
+      || (!reste && game->map[(int)game->p->pos_y][(int)vector_x - 1] == 'D'))
 			game->p->pos_x += 0.1;
 	}
-	if (game->map[(int)vector_y][(int)game->p->pos_x] != '1' && game->map[(int)vector_y][(int)game->p->pos_x] != 'D')
+	if (game->map[(int)vector_y][(int)game->p->pos_x] != '1'
+    && game->map[(int)vector_y][(int)game->p->pos_x] != 'D')
 	{
 		game->p->pos_y = vector_y;
 		reste = modf(vector_y, &unused);
-		if ((!reste && game->map[(int)vector_y - 1][(int)game->p->pos_x] == '1') || (!reste && game->map[(int)vector_y - 1][(int)game->p->pos_x] == 'D'))
+		if ((!reste && game->map[(int)vector_y - 1][(int)game->p->pos_x] == '1')
+      || (!reste && game->map[(int)vector_y - 1][(int)game->p->pos_x] == 'D'))
 			game->p->pos_y += 0.1;
 	}
 	rendering(game);
@@ -43,14 +47,8 @@ void	move(t_game *game, double vector_x, double vector_y)
 void	move_player(t_game *game)
 {
 	if (game->key_bool[0])
-	{
-		if (!is_wall_or_door(game->map[(int)game->p->pos_y]
-			[(int)(game->p->pos_x + game->p->dir_x * MOVESPEED)]))
-			game->p->pos_x += game->p->dir_x * MOVESPEED;
-		if (!is_wall_or_door(game->map[(int)(game->p->pos_y + game->p->dir_y
-			* MOVESPEED)][(int)game->p->pos_x]))
-			game->p->pos_y += game->p->dir_y * MOVESPEED;
-	}
+		move(game, game->p->pos_x + game->p->dir_x * MOVESPEED,
+			game->p->pos_y + game->p->dir_y * MOVESPEED);
 	if (game->key_bool[1])
 		move(game, game->p->pos_x - game->p->dir_x * MOVESPEED,
 			game->p->pos_y - game->p->dir_y * MOVESPEED);
